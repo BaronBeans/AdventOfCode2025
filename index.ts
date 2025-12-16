7fb86af388227659d5717cdd0310fe6b770ec67b
barnars <sean.barnard@autodesk.com>
2025-12-16 12:04:11 +0000

    init

---
+++ index.ts
@@ -0,0 +1,38 @@
+import readline from "readline";
+import { checkDay, setupDay } from "./init";
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+});
+
+rl.question("Enter day number: ", async (answer) => {
+    if (answer === "") {
+        console.log("Getting current date");
+        const day = new Date().getDate();
+        answer = day.toString();
+    } else {
+        if (Number(answer) > 30 || answer === "" || isNaN(Number(answer))) {
+            console.log("Invalid day number");
+
+        } else {
+            const day = Number(answer);
+            console.log(`Day: ${day}`);
+        }
+    }
+    rl.close();
+
+    if (checkDay(Number(answer))) {
+        await runDay(Number(answer));
+    } else {
+        setupDay(Number(answer));
+        await runDay(Number(answer));
+    }
+});
+
+const runDay = async (day: number) => {
+    const dayFile = `./solutions/day${day}/day${day}.ts`;
+    const dayModule = await import(dayFile);
+    dayModule.part1();
+    dayModule.part2();
+}
---
+++ init.ts
@@ -0,0 +1,24 @@
+import fs from "fs";
+
+export const checkDay = (day: number) => {
+    const dayFolder = `./solutions/day${day}`;
+    const dayFile = `${dayFolder}/day${day}.ts`;
+    if (!fs.existsSync(dayFile)) {
+        return false;
+    }
+    return true;
+};
+
+export const setupDay = (day: number) => {
+    const dayFolder = `./solutions/day${day}`;
+    const dayFile = `${dayFolder}/day${day}.ts`;
+    // const dayInput = `${dayFolder}/day${day}.input`;
+    // const dayOutput = `${dayFolder}/day${day}.output`;
+
+    if (!fs.existsSync(dayFolder)) {
+        fs.mkdirSync(dayFolder);
+    }
+    if (!fs.existsSync(dayFile)) {
+        fs.writeFileSync(dayFile, fs.readFileSync(`./template.txt`, 'utf8'));
+    }
+};
